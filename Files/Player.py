d58334247703a5feae5eb9bb8887c7d7a5224d9b
Dennis Co <c0d3nnis@gmail.com>
2012-10-19 14:43:27 -0400

    Added ability toggle

--- Files/Player.py
+++ Files/Player.py
@@ -4,8 +4,8 @@ import math
 
 class Player(object):
   def __init__(self):
-    self.speed = 70
-    self.lightDist = 20
+    self.speed = 100
+    self.lightDist = 15
     self.forward = Vec3(0,1,0)
     self.back = Vec3(0,-1,0)
     self.left = Vec3(-1,0,0)
@@ -22,20 +22,40 @@ class Player(object):
     self.keyMap['left'] = 0
     self.keyMap['right'] = 0
     self.keyMap['back'] = 0
+    self.abilities = {}
+    self.abilities['wall'] = 0
+    self.abilities['light'] = 0
     
   #Set key controls
   def initControls(self):
-    base.accept("w", self.setKey, ["forward", 1])
-    base.accept("a", self.setKey, ["left", 1])
-    base.accept("d", self.setKey, ["right", 1])
-    base.accept("s", self.setKey, ["back", 1])
-    base.accept("w-up", self.setKey, ["forward", 0])
-    base.accept("a-up", self.setKey, ["left", 0])
-    base.accept("d-up", self.setKey, ["right", 0])
-    base.accept("s-up", self.setKey, ["back", 0])
+    #Movement
+    base.accept('w', self.setKey, ['forward', 1])
+    base.accept('a', self.setKey, ['left', 1])
+    base.accept('d', self.setKey, ['right', 1])
+    base.accept('s', self.setKey, ['back', 1])
+    base.accept('w-up', self.setKey, ['forward', 0])
+    base.accept('a-up', self.setKey, ['left', 0])
+    base.accept('d-up', self.setKey, ['right', 0])
+    base.accept('s-up', self.setKey, ['back', 0])
+    #Abilities
+    base.accept('1', self.toggleKey, ['wall'])
+    base.accept('2', self.toggleKey, ['light'])
+    base.accept('f', self.cancelKey)
         
   def setKey(self, key, value):
     self.keyMap[key] = value
+  
+  #Toggles ability
+  def toggleKey(self, key):
+    for ability in self.abilities.keys():
+      if ability != key:
+        self.abilities[ability] = 0
+    self.abilities[key] = (self.abilities[key] + 1) % 3
+    
+  #Cancels ability
+  def cancelKey(self):
+    for ability in self.abilities.keys():
+      self.abilities[ability] = 0
     
   #Loads player node, camera, and light
   def initPlayer(self):
@@ -50,20 +70,19 @@ class Player(object):
     base.cam.node().setLens(lens)
     base.camera.reparentTo(self.playerNode)
     
-    """
-    #Testing light
+    #Testing light location
     self.test = loader.loadModel('Models/sphere')
     self.test.reparentTo(self.playerNode)
     self.test.setTwoSided(True)
-    self.test.setPos(self.lightPos)
-    """
-    self.pLightNode = NodePath("light-node")
+    self.test.setPos(Vec3(0,self.lightDist,0))
+    
+    #Loads artifact point light
+    self.pLightNode = NodePath('light-node')
     self.pLightNode.reparentTo(self.playerNode)
     self.pLightNode.setPos(Vec3(0,self.lightDist,0))
-    #Loads artifact point light
     pLight = PointLight('player-light')
     pLightNP = self.pLightNode.attachNewNode(pLight)
-    pLightNP.node().setColor(Vec4(0.2, 0.2, 0.2, 1.0))
+    pLightNP.node().setColor(Vec4(0.1, 0.2, 0.2, 1.0))
     pLightNP.node().setAttenuation(Vec3(0, 0.001, 0.0001))
     
     render.setLight(pLightNP)
@@ -71,13 +90,16 @@ class Player(object):
   def initCollisions(self, pusher, cHandler):
     #Collide with env
     cNode = CollisionNode('player')
-    cSphere = CollisionSphere(0, 0, 2, 3)
+    cSphere = CollisionSphere(0, 0, 4, 10)
     cNode.addSolid(cSphere)
     cNodePath = self.playerNode.attachNewNode(cNode)
     cNodePath.show()
     base.cTrav.addCollider(cNodePath, pusher)
     pusher.addCollider(cNodePath, self.playerNode, base.drive.node())
-    
+  
+  def update(self, dt):
+    self.move(dt)    
+  
   #Moves player
   def move(self, dt):
     #Change heading based on mouse movement
@@ -89,17 +111,20 @@ class Player(object):
       cam_p = base.camera.getP() - (y - base.win.getYSize()/2)*.1
       if cam_p >= -90 and cam_p <= 90:
         base.camera.setP(cam_p)
+        
+    self.pLightNode.setPos(0,self.lightDist*math.cos(deg2Rad(base.camera.getP())),
+                           self.lightDist*math.sin(deg2Rad(base.camera.getP())))
+    self.test.setPos(self.pLightNode.getPos())
     
-    self.pLightNode.setPos(0,self.lightDist,self.lightDist*math.sin(deg2Rad(base.camera.getP())))
     #Move player based on key movements
-    if self.keyMap["forward"] == 1:
+    if self.keyMap['forward'] == 1:
       self.playerNode.setPos(self.playerNode, self.forward * dt * self.speed)
-    elif self.keyMap["back"] == 1:
+    elif self.keyMap['back'] == 1:
       self.playerNode.setPos(self.playerNode, self.back * dt * self.speed)
     
-    if self.keyMap["left"] == 1:
+    if self.keyMap['left'] == 1:
       self.playerNode.setPos(self.playerNode, self.left * dt * self.speed)
-    elif self.keyMap["right"] == 1:
+    elif self.keyMap['right'] == 1:
       self.playerNode.setPos(self.playerNode, self.right * dt * self.speed)
   
     
