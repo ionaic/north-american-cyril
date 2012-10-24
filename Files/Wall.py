06042b4c9ea9620d2ce45cd48f9f98b809ecb8f7
Ian Ooi <ionaic@gmail.com>
2012-10-24 15:39:21 -0400

    stuff and things

--- Files/Wall.py
+++
@@ -1,7 +0,0 @@
-from pandac.PandaModules import * #basic Panda modules
-from direct.showbase.DirectObject import DirectObject #event handling
-
-class Wall:
-    def __init__(self, modelPath = ""):
-        self.model = None # wall model nodepath
-        
--- src/Wall.py
+++
@@ -1,4 +0,0 @@
-from pandac.PandaModules import * #basic Panda module
-from direct.showbase.DirectObject import DirectObject #event handling
-
-class Wall(
