16fa97a0a4fc750a6d7012e2b4d41745026a4312
Dennis Co <c0d3nnis@gmail.com>
2012-10-31 00:39:33 -0400

    New map gen

--- Files/Play.py
+++ Files/Play.py
@@ -86,8 +86,9 @@ class Play(DirectObject):
       self.playingBGM.play()
     
   def initModels(self):
-    self.map = MapGen(self)
+    #self.map = MapGen(self)
     self.player = Player(self)
+    self.level = Level(self, 
     self.enemies = []
     self.level = 1
     self.startLevel(1)
